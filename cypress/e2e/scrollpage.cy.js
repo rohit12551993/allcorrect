46695c6aee924fb0847c62548316dd4b39b8575a
rohit12551993 <rohitmadan1993@mail.com>
2025-05-12 21:32:42 +0530

    right

--- cypress/e2e/scrollpage.cy.js
+++ cypress/e2e/scrollpage.cy.js
@@ -2,6 +2,7 @@ describe("scroll page",()=>{
     it("This will scroll the page",()=>{
         //This will open the website
     cy.visit('https://testautomationpractice.blogspot.com/');
+    
 
     cy.get("div[id='HTML1'] h2[class='title']").scrollIntoView();
     
